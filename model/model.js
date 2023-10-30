0775b8b5c9fecf87d13fb67c19c4eb8d670d510b
RamadeviRamkumar <ramdevcse25@gmail.com>
2023-10-30 18:41:40 +0530

    n

--- model/model.js
+++ model/model.js
@@ -1,33 +1,34 @@
-var mongoose = require ('mongoose');
+var mongoose = require("mongoose");
 
-var Schema = mongoose.Schema ({
-    Empname :{
-        required : true,
-        type : String
-    },
-    Empemail : {
-        required : true,
-        type : String
-    },
-    Phonenumber : {
-        required : true,
-        type : Number
-    },
-    Password : {
-        required : false,
-        type : String
-    },
-    created_at : {
-        type : Date,
-        default : Date.now
-    },
-})
-Schema.path('Empemail').validate(async () =>{
-    const emailCount = await mongoose.model.Details.countDocuments({Empemail})
-    return !emailCount
-  },'Empemail already exists');
-  
-  var Signup = module.exports = mongoose.model('Details',Schema);
-module.exports.get = function(callback, limit) {
-    Signup.find(callback).limit(limit);
+var Schema = mongoose.Schema({
+  Empname: {
+    required: true,
+    type: String,
+  },
+  Empemail: {
+    required: true,
+    type: String,
+  },
+  Phonenumber: {
+    required: true,
+    type: Number,
+  },
+  Password: {
+    required: false,
+    type: String,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// Schema.path("Empemail").validate(async () => {
+//   const emailCount = await mongoose.models.Details.countDocuments({ Empemail });
+//   console.log(Empemail);
+//   return !emailCount;
+// }, "Empemail already exists");
+
+var Signup = (module.exports = mongoose.model("Details", Schema));
+module.exports.get = function (callback, limit) {
+  Signup.find(callback).limit(limit);
 };
